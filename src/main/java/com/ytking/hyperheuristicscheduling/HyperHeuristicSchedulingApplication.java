44bc310d08af1c2a5546858bedbe00cb01c1eb31
应涛 <yt1577396227@163.com>
2021-12-13 21:08:13 +0800

    Initial commit

---
+++ src/main/java/com/ytking/hyperheuristicscheduling/HyperHeuristicSchedulingApplication.java
@@ -0,0 +1,13 @@
+package com.ytking.hyperheuristicscheduling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyperHeuristicSchedulingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyperHeuristicSchedulingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ytking/hyperheuristicscheduling/HyperHeuristicSchedulingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ytking.hyperheuristicscheduling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyperHeuristicSchedulingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
